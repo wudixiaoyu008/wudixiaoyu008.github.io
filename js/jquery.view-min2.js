2d958856e7a6f3772a7acafc76ba8bf333a7c950
wudixiaoyu008 <liuyuc@outlook.com>
2021-03-08 16:26:47 -0800

    updated empty

--- js/jquery.view-min2.js
+++
@@ -1,9 +0,0 @@
-    // var regex = /php\d+@@+\d[abc]/;
-    var regex = /yu-design/;
-    var pwd = window.prompt("Please type password: ", '');
-    if (regex.test(pwd)){
-    }
-    else {
-        alert("Hmmm, please contact me in person for access");
-        window.location = "../index.html";
-    }
